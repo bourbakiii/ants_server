d9bbdc97dfee5b863ebb45428784dbc9fb646768
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-06-30 18:59:05 +0300

    get local users request

--- Settings/UsersController.js
+++ Settings/UsersController.js
@@ -4,5 +4,5 @@ const response = require("../response");
 const users = [{id: 1, name: "Анаида"}, {id: 2, name: "Мария"}];
 
 exports.get = (req, res) => {
-    response.status(users, res);
+    response.status(users, res)
 }
